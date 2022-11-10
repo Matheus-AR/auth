6ee98c6dabcbfce635b1403d202be0874f607299
HC4MAC29 <hc4mac29@HC4MAC29.local>
2022-11-10 19:33:59 -0300

    Corrigindo as duas funcoes do userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 
 async function registrar(req, res) {
     const user = new User(req.body);
-    user.password = await bcrypt.hashSync(user.password, 8);
+    user.password = bcrypt.hashSync(user.password, 8);
     await user.save()
         .then(doc => {
             doc.password = undefined;
@@ -11,7 +11,7 @@ async function registrar(req, res) {
         })
         .catch(error => {
             const msg = {};
-            Object.values(error.errors).forEach(({properties}) => {
+            Object.values(error.errors).forEach(({ properties }) => {
                 msg[properties.path] = properties.message;
             });
             if (error.code == 11000) {
@@ -23,20 +23,20 @@ async function registrar(req, res) {
 
 async function login(req, res) {
     const { email, password } = req.body;
-    await User.findOne({email: email}).select("+password")
-    .then(async doc => {
-        if (!doc) {
-            return res.status(404).json({ erro: 'Usuário não cadastrado!' });
-        }
-        const autentica = await bcrypt.compareSync(password, doc.password);
-        if (!autentica) {
-            return res.status(400).json({ erro: "Senha inválida" })
-        }
-        return res.status(200).json({ msg: "Usuário logado"});
-    })
-    .catch(error => {
-        return res.status(500).json(error);
-    });
+    await User.findOne({ email: email }).select("+password")
+        .then(async doc => {
+            if (!doc) {
+                return res.status(404).json({ erro: 'Usuário não cadastrado!' });
+            }
+            const autentica = bcrypt.compareSync(password, doc.password);
+            if (!autentica) {
+                return res.status(400).json({ erro: "Senha inválida" })
+            }
+            return res.json({email: email, token: "abcd"});
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
 }
 
 module.exports = { registrar, login }
