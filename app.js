5a7def4007bbc64f7e58675c7041e0489edf264e
HC4MAC29 <hc4mac29@HC4MAC29.local>
2022-11-10 20:34:38 -0300

    adicionando o middleware da rota de produtos

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/env.json');
 const userRoute = require('./routes/userRoute');
-
+const produtoRoute = require('./routes/produtoRoute');
 const app = express();
 
 app.use(express.json());
 
 app.use('/users', userRoute);
+app.use('/produtos', produtoRoute);
 
 mongoose.connect(config.url)
     .then(app.listen(config.porta, () => {
